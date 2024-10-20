66d778e44de1e78a8be33d882875dd7a7f6e7451
Juned-the-programmer <davdajuned7698@gmail.com>
2024-10-20 19:33:29 +0530

    Customer Module Frontend

--- Inventory/urls.py
+++ Inventory/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Dashboard.urls')),
-    path('auth', include('pages.urls'))
+    path('auth', include('pages.urls')),
+    path('customer/', include('customer.urls'))
 ]
